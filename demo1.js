97a1d70e0c7255537b3a68e2a3fabab17c8384d0
Blade <86.shinelnozen@gmail.com>
2024-08-18 20:50:08 +0800

    WebJs

---
+++ demo1.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-08-18
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.bilibili.com/video/BV1Da411Z7s7/?spm_id_from=333.1007.top_right_bar_window_custom_collection.content.click&vd_source=0f518c19a11f1b93ce3e91b7d6576a80
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
