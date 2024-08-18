3e73a2c2a204f10b7c521f121471be91b38e98bf
Blade <86.shinelnozen@gmail.com>
2024-08-18 22:02:10 +0800

    version1.2

--- 点击去学习后.js
+++ 点击去学习后.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         手动触发点击链接
+// @name         手动触发点击多个链接
 // @namespace    http://tampermonkey.net/
-// @version      1.5
-// @description  添加按钮，通过点击按钮来触发自动点击目标<a>链接的功能
+// @version      1.6
+// @description  添加按钮，通过点击按钮来触发自动点击所有匹配的<a>链接的功能
 // @author       你的名字
 // @match        https://www.teachersedu.cn/project/projectcourselist?projectid=588614481471341829
 // @grant        none
@@ -13,7 +13,7 @@
 
     // 创建并添加一个按钮到页面
     var customButton = document.createElement('button');
-    customButton.innerText = '点击链接';
+    customButton.innerText = '点击所有链接';
     customButton.style.position = 'fixed';
     customButton.style.top = '10px';
     customButton.style.right = '10px';
@@ -29,23 +29,27 @@
 
     // 设置按钮点击事件
     customButton.addEventListener('click', function() {
-        console.log('按钮被点击，开始自动点击目标链接');
+        console.log('按钮被点击，开始自动点击所有目标链接');
 
-        // 开始执行点击目标链接的功能
-        clickTargetLink();
+        // 开始执行点击多个目标链接的功能
+        clickAllTargetLinks();
     });
 
-    // 自动点击目标链接的函数
-    function clickTargetLink() {
-        // 使用你提供的选择器定位到目标<a>元素
-        var targetLink = document.querySelector('#teacher > div:nth-child(3) > div > div.col-lg-9.col-md-8.course-detail-content > section > div:nth-child(2) > div.panel-body > div > div > div:nth-child(1) > div.el-tree-node__content > ul > li > a');
+    // 自动点击多个目标链接的函数
+    function clickAllTargetLinks() {
+        // 使用选择器定位所有目标<a>元素
+        var targetLinks = document.querySelectorAll('#teacher > div:nth-child(3) > div > div.col-lg-9.col-md-8.course-detail-content > section > div:nth-child(2) > div.panel-body > div > div > div:nth-child(1) > div.el-tree-node__content > ul > li > a');
 
         // 检查是否找到了目标链接
-        if (targetLink) {
-            targetLink.click();
-            console.log('找到并点击了目标<a>元素');
+        if (targetLinks.length > 0) {
+            targetLinks.forEach(function(link, index) {
+                setTimeout(function() {
+                    link.click();
+                    console.log('点击了第 ' + (index + 1) + ' 个链接');
+                }, index * 1000); // 每次点击之间添加1秒的延迟
+            });
         } else {
-            console.log('未找到目标<a>元素');
+            console.log('未找到任何目标<a>元素');
         }
     }
 
