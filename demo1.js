a3f4f2b5abc5bad074cde82fe1ff045fd9c4348d
Blade <86.shinelnozen@gmail.com>
2024-08-18 21:01:13 +0800

    version1.0

--- demo1.js
+++ demo1.js
@@ -1,16 +1,35 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         自动点击按钮
 // @namespace    http://tampermonkey.net/
-// @version      2024-08-18
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.bilibili.com/video/BV1Da411Z7s7/?spm_id_from=333.1007.top_right_bar_window_custom_collection.content.click&vd_source=0f518c19a11f1b93ce3e91b7d6576a80
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @version      1.0
+// @description  自动点击网页上的指定按钮
+// @author       你的名字
+// @match        https://www.teachersedu.cn/project/projectcourselist?projectid=588614481471341829
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Your code here...
+    // 等待页面加载完成
+    window.onload = function() {
+        // 设置延迟时间（毫秒）
+        var delay = 3000; // 3秒
+
+        // 使用setTimeout来实现延迟点击
+        setTimeout(function() {
+            // 使用CSS选择器找到按钮元素
+            var button = document.querySelector('#myButton'); // 替换为实际的选择器
+            
+            // 检查按钮是否存在
+            if (button) {
+                // 自动点击按钮
+                button.click();
+                console.log('按钮已被自动点击');
+            } else {
+                console.log('未找到按钮');
+            }
+        }, delay); // 延迟时间
+    };
+
 })();
